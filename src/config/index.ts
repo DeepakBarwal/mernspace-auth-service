c739b4115b009e4c53e2eb7e2d844d2d355de643
Deepak Barwal <deepakbarwal98@gmail.com>
2024-12-15 17:15:42 +0530

    chore: setup typeorm

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,16 @@
+import path from 'node:path'
 import { config } from 'dotenv'
-config()
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) })
 
-const { PORT, NODE_ENV } = process.env
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+  process.env
 
 export const Config = {
   PORT,
-  NODE_ENV
+  NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME
 }
