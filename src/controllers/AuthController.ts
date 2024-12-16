e16d5502205d90b050371ac85a41e99777b7c8b9
Deepak Barwal <deepakbarwal98@gmail.com>
2024-12-16 19:05:30 +0530

    add: logger for register

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,16 +1,23 @@
 import { NextFunction, Response } from 'express'
 import { RegisterUserRequest } from '../types'
 import { UserService } from '../services/UserService'
+import { Logger } from 'winston'
 
 export class AuthController {
-  userService: UserService
-
-  constructor(userService: UserService) {
-    this.userService = userService
-  }
+  constructor(
+    private userService: UserService,
+    private logger: Logger
+  ) {}
 
   async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body
+
+    this.logger.debug('New request to register a user', {
+      firstName,
+      lastName,
+      email,
+      password: '******'
+    })
 
     try {
       const createdUser = await this.userService.create({
@@ -19,6 +26,8 @@ export class AuthController {
         email,
         password
       })
+
+      this.logger.info('User has been registered', { id: createdUser.id })
 
       res.status(201).json({ id: createdUser.id })
     } catch (error) {
