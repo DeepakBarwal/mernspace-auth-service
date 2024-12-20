df3931cb800f8177c29ef8dd3c521fa06c67faf0
Deepak Barwal <deepakbarwal98@gmail.com>
2024-12-20 21:49:15 +0530

    chore: refactor token service using DI

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,8 +4,6 @@ import { validationResult } from 'express-validator'
 import { JwtPayload } from 'jsonwebtoken'
 import { RegisterUserRequest } from '../types'
 import { UserService } from '../services/UserService'
-import { AppDataSource } from '../config/data-source'
-import { RefreshToken } from '../entity/RefreshToken'
 import { TokenService } from '../services/TokenService'
 
 export class AuthController {
@@ -51,12 +49,8 @@ export class AuthController {
       const accessToken = this.tokenService.generateAccessToken(payload)
 
       // Persist the refresh token
-      const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
-      const refreshTokenRepository = AppDataSource.getRepository(RefreshToken)
-      const newRefreshToken = await refreshTokenRepository.save({
-        user: createdUser,
-        expiresAt: new Date(Date.now() + MS_IN_YEAR)
-      })
+      const newRefreshToken =
+        await this.tokenService.persistRefreshToken(createdUser)
 
       const refreshToken = this.tokenService.generateRefreshToken({
         ...payload,
