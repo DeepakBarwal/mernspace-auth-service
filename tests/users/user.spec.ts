59e7d225b872e5b02c2139af30d85e3bb55a30be
Deepak Barwal <deepakbarwal98@gmail.com>
2024-12-24 21:51:19 +0530

    add: test for user data

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -1,22 +1,28 @@
 import { DataSource } from 'typeorm'
 import request from 'supertest'
-import bcrypt from 'bcrypt'
+import createJWKSMock from 'mock-jwks'
 import { AppDataSource } from '../../src/config/data-source'
+import app from '../../src/app'
 import { User } from '../../src/entity/User'
 import { Roles } from '../../src/constants'
-import app from '../../src/app'
-import { isJwt } from '../utils'
 
 describe('GET /auth/self', () => {
   let connection: DataSource
+  let jwks: ReturnType<typeof createJWKSMock>
 
   beforeAll(async () => {
+    jwks = createJWKSMock('http://localhost:5501')
     connection = await AppDataSource.initialize()
   })
 
   beforeEach(async () => {
+    jwks.start()
     await connection.dropDatabase()
     await connection.synchronize()
+  })
+
+  afterEach(() => {
+    jwks.stop()
   })
 
   afterAll(async () => {
@@ -26,8 +32,36 @@ describe('GET /auth/self', () => {
   describe('Given all fields', () => {
     it('should return the 200 status code', async () => {
       const response = await request(app).get('/auth/self').send()
-
       expect(response.statusCode).toBe(200)
+    })
+
+    it('should return the user data', async () => {
+      // Register user
+      const userData = {
+        firstName: 'Deepak',
+        lastName: 'Barwal',
+        email: 'deepak@mern.space',
+        password: 'password'
+      }
+      const userRepository = connection.getRepository(User)
+      const data = await userRepository.save({
+        ...userData,
+        role: Roles.CUSTOMER
+      })
+
+      // Generate token
+      const accessToken = jwks.token({ sub: String(data.id), role: data.role })
+
+      // Add token to cookie
+
+      const response = await request(app)
+        .get('/auth/self')
+        .set('Cookie', [`accessToken=${accessToken};`])
+        .send()
+
+      // Assert
+      // check if user id matches with registered user
+      expect((response.body as Record<string, string>).id).toBe(data.id)
     })
   })
 })
