8d0df8bd775758643e679689f1c6b6d68f271b14
Deepak Barwal <deepakbarwal98@gmail.com>
2025-01-01 20:33:16 +0530

    add: tenant validator, get all tenants, get/update/delete tenant by id

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -1,7 +1,9 @@
-import { NextFunction, Response } from 'express'
+import { Request, NextFunction, Response } from 'express'
 import { TenantService } from '../services/TenantService'
 import { CreateTenantRequest, ITenant } from '../types'
 import { Logger } from 'winston'
+import { validationResult } from 'express-validator'
+import createHttpError from 'http-errors'
 
 export class TenantController {
   constructor(
@@ -20,6 +22,86 @@ export class TenantController {
     } catch (error) {
       next(error)
       return
+    }
+  }
+
+  async update(req: CreateTenantRequest, res: Response, next: NextFunction) {
+    // Validation
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      res.status(400).json({ errors: result.array() })
+      return
+    }
+    const { name, address } = req.body
+    const tenantId = req.params.id
+    if (isNaN(Number(tenantId))) {
+      next(createHttpError(400, 'Invalid url param.'))
+      return
+    }
+    this.logger.debug('Request for updating a tenant', req.body)
+    try {
+      await this.tenantService.update(Number(tenantId), {
+        name,
+        address
+      })
+      this.logger.info('Tenant has been updated', { id: tenantId })
+      res.json({ id: Number(tenantId) })
+    } catch (err) {
+      next(err)
+      return
+    }
+  }
+
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tenants = await this.tenantService.getAll()
+      this.logger.info('All tenant have been fetched')
+      res.json(tenants)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  async getOne(req: Request, res: Response, next: NextFunction) {
+    const tenantId = req.params.id
+
+    if (isNaN(Number(tenantId))) {
+      next(createHttpError(400, 'Invalid url param.'))
+      return
+    }
+
+    try {
+      const tenant = await this.tenantService.getById(Number(tenantId))
+
+      if (!tenant) {
+        next(createHttpError(400, 'Tenant does not exist.'))
+        return
+      }
+
+      this.logger.info('Tenant has been fetched')
+      res.json(tenant)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  async destroy(req: Request, res: Response, next: NextFunction) {
+    const tenantId = req.params.id
+
+    if (isNaN(Number(tenantId))) {
+      next(createHttpError(400, 'Invalid url param.'))
+      return
+    }
+
+    try {
+      await this.tenantService.deleteById(Number(tenantId))
+
+      this.logger.info('Tenant has been deleted', {
+        id: Number(tenantId)
+      })
+      res.json({ id: Number(tenantId) })
+    } catch (err) {
+      next(err)
     }
   }
 }
